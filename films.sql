2e82f33814906ef21024e9845318cbd824ef6af8
Sergey <50179609+ED4M@users.noreply.github.com>
2020-10-05 13:00:20 +0300

    Update films.sql

--- films.sql
+++ films.sql
@@ -20,21 +20,21 @@ CREATE TABLE IF NOT EXISTS  HW1.PERSONS2CONTENT(
   PERSON_TYPE VARCHAR (25)
 );
 
- INSERT INTO DZ1.FILMS (TITLE, ID, COUNTRY, BOX_OFFICE, RELEASE_DATE) VALUES
+ INSERT INTO HW1.FILMS (TITLE, ID, COUNTRY, BOX_OFFICE, RELEASE_DATE) VALUES
   ('Начало', 1, 'США, Великобритания', 882332032, 'Jul 8,2010'),
   ('Маяк', 2, 'Канада, США', 4000000, 'May 19,2019'),
   ('Интерстеллар', 3, 'США, Великобритания, Канада', 677463813, 'Oct 26,2014'),
   ('Иллюзия обмана', 4, 'США, Франция', 351723989, 'May 21,2013'),
   ('Остров проклятых', 5, 'США', 294804195, 'Feb 13,2010');
     
-INSERT INTO DZ1.PERSONS ( ID,  FIO)  VALUES 
+INSERT INTO HW1.PERSONS ( ID,  FIO)  VALUES 
   ( 1, 'Нолан Кристофер'), 
   ( 2, 'Роберт Эггерс'), 
   ( 3, 'Ханс Циммер'), 
   ( 4, 'Вуди Харрельсон'), 
   ( 5, 'Мартин Скорсезе');
   
-INSERT INTO DZ1.PERSONS2CONTENT (  PERSON_ID, FILM_ID, PERSON_TYPE) VALUES 
+INSERT INTO HW1.PERSONS2CONTENT (  PERSON_ID, FILM_ID, PERSON_TYPE) VALUES 
 (1, 1, 'Режиссёр'), 
 (2, 2, 'Режиссёр'), 
 (3, 3, 'Композитор'), 
